ba13bc756e023ae9bac8084970eada8fdbccc017
Artiwarah Damrongchai <artiwarahd@gmail.com>
2014-01-30 21:50:36 +0700

    #64760136
    Application layout

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,16 @@
 class HomesController < ApplicationController
+  before_filter :load_main_nav, only: :index
+
+  def load_main_nav
+    @main_nav = {
+      "Home" => "landing",
+      "Portfolio" => "portfolio",
+      "About me" => "about-me",
+      "Contact" => "contact",
+      "Links" => "links"
+    }
+  end
+
   def index
     
   end
