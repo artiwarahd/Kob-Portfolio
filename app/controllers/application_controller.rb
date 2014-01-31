16d7ae95afc81ea6425b38f9ce2e498f3f240920
Artiwarah Damrongchai <artiwarah@iwa.fi>
2014-01-31 13:30:53 +0700

    Portfolio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :load_main_nav
+
+  def load_main_nav
+    @main_nav = {
+      "Home" => "landing",
+      "Portfolio" => "portfolio",
+      "About me" => "about-me",
+      "Contact" => "contact",
+      "Links" => "links"
+    }
+  end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,16 +1,4 @@
 class HomesController < ApplicationController
-  before_filter :load_main_nav, only: :index
-
-  def load_main_nav
-    @main_nav = {
-      "Home" => "landing",
-      "Portfolio" => "portfolio",
-      "About me" => "about-me",
-      "Contact" => "contact",
-      "Links" => "links"
-    }
-  end
-
   def index
     
   end
