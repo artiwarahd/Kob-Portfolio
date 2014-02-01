43ee258e12db361c2db6283ca2e10daff845b76c
Artiwarah Damrongchai <artiwarahd@gmail.com>
2014-02-01 22:15:10 +0700

    add left menu
    contact information pafe

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,15 +1,15 @@
 Globals = {
-    animateAppear: function(selector, direction){
-        $(selector).toggle('slide', {
-            direction: direction
-        }, 500);
-    },
-    init: function(){
-        // Use with jQuery waypoints
-        // https://github.com/imakewebthings/jquery-waypoints
-        // Globals.animateAppear(".left", "left");
-        // Globals.animateAppear(".right", "right");
-    }
+  animateAppear: function(selector, direction){
+    $(selector).toggle('slide', {
+        direction: direction
+    }, 500);
+  },
+  init: function(){
+    // Use with jQuery waypoints
+    // https://github.com/imakewebthings/jquery-waypoints
+    // Globals.animateAppear(".left", "left");
+    // Globals.animateAppear(".right", "right");
+  }
 };
 
 $(Globals.init);
