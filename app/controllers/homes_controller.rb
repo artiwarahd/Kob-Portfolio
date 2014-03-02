39127d1acdb5ecf661f3e39921eef9aa623e692f
Artiwarah Damrongchai <artiwarahd@gmail.com>
2014-03-02 22:09:38 +0700

    resize images
    isotope css

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,12 +2,12 @@ class HomesController < ApplicationController
   def index
     @images = {
       "job" => {
-        "archen" => Dir.glob("app/assets/images/folio/job/archen/*.jpg"),
-        "arsomsilp" => Dir.glob("app/assets/images/folio/job/arsomsilp/*.jpg")
+        "archen" => Dir.glob("app/assets/images/folio/job/archen/*_thumb.jpg"),
+        "arsomsilp" => Dir.glob("app/assets/images/folio/job/arsomsilp/*_thumb.jpg")
       },
       "study" => {
-        "general" => Dir.glob("app/assets/images/folio/study/*.jpg"),
-        "thesis" => Dir.glob("app/assets/images/folio/study/thesis/*.jpg")
+        "general" => Dir.glob("app/assets/images/folio/study/*_thumb.jpg"),
+        "thesis" => Dir.glob("app/assets/images/folio/study/thesis/*_thumb.jpg")
       }
     }
   end
