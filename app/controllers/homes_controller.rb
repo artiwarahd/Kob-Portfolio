ebb2651abcba119eccfb83368dc5270f44adaabb
Artiwarah Damrongchai <artiwarahd@gmail.com>
2014-03-07 00:23:42 +0700

    add arsromsilp works
    modify UI

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,12 +2,22 @@ class HomesController < ApplicationController
   def index
     @images = {
       "job" => {
-        "archen" => Dir.glob("app/assets/images/folio/job/archen/*_thumb.jpg"),
-        "arsomsilp" => Dir.glob("app/assets/images/folio/job/arsomsilp/*_thumb.jpg")
+        "archen" => Dir.glob("app/assets/images/folio/job/archen/*_thumb.jpg")
       },
       "study" => {
         "general" => Dir.glob("app/assets/images/folio/study/*_thumb.jpg"),
         "thesis" => Dir.glob("app/assets/images/folio/study/thesis/*_thumb.jpg")
+      }
+    }
+
+    @arsomsilp_works = {
+      "hostpice" => {
+        "cover" => Dir.glob("app/assets/images/folio/job/arsomsilp/PLATE28.jpg"),
+        "images" => Dir.glob("app/assets/images/folio/job/arsomsilp/hostpice/*.jpg")
+      },
+      "T.house" => {
+        "cover" => Dir.glob("app/assets/images/folio/job/arsomsilp/PLATE26.jpg"),
+        "images" => Dir.glob("app/assets/images/folio/job/arsomsilp/T.house/*.jpg")
       }
     }
   end
