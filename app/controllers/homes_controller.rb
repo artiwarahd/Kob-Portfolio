f8bad42e97f1124b321f270d01eea86c791c3b46
Artiwarah Damrongchai <artiwarahd@gmail.com>
2014-03-09 22:27:06 +0700

    add honor group
    always show next-prev buttons

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,18 +5,24 @@ class HomesController < ApplicationController
         "archen" => Dir.glob("app/assets/images/folio/job/archen/*_thumb.jpg")
       },
       "study" => {
-        "general" => Dir.glob("app/assets/images/folio/study/*_thumb.jpg"),
-        "thesis" => Dir.glob("app/assets/images/folio/study/thesis/*_thumb.jpg")
+        "general" => Dir.glob("app/assets/images/folio/study/*_thumb.jpg")
       }
+    }
+
+    @honor = Dir.glob("app/assets/images/folio/study/honor/*_thumb.jpg")
+
+    @thesis = {
+      "cover" => "folio/study/thesis/COVER11.jpg",
+      "images" => Dir.glob("app/assets/images/folio/study/thesis/PLATE*.jpg")
     }
 
     @arsomsilp_works = {
       "hostpice" => {
-        "cover" => Dir.glob("app/assets/images/folio/job/arsomsilp/PLATE28.jpg"),
+        "cover" => "folio/job/arsomsilp/PLATE28.jpg",
         "images" => Dir.glob("app/assets/images/folio/job/arsomsilp/hostpice/*.jpg")
       },
       "T.house" => {
-        "cover" => Dir.glob("app/assets/images/folio/job/arsomsilp/PLATE26.jpg"),
+        "cover" => "folio/job/arsomsilp/PLATE26.jpg",
         "images" => Dir.glob("app/assets/images/folio/job/arsomsilp/T.house/*.jpg")
       }
     }
